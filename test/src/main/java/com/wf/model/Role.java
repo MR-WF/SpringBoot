e13684369054bb76e3ad18df541eeeeb85cc18ed
It_Wf <it_wangfeng@126.com>
2018-07-01 23:58:56 +0800

    递归常用应用-树形组织结构

--- test/src/main/java/com/wf/model/Role.java
+++ test/src/main/java/com/wf/model/Role.java
@@ -4,6 +4,9 @@ package com.wf.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 /**
  * Created by Mr_WF on 2018/6/30.
@@ -13,9 +16,8 @@ import lombok.Setter;
 public class Role {
     private Long id;
     private String name;
+    private Role parent;
+    private Set<Role> children = new HashSet<Role>();
 
-    public Role(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+
 }
